1a3cec1b05f6847ab450be7d56dd72be9e0f8b03
Muhammad Asif <asif88383@gmail.com>
2022-04-03 03:26:28 -0700

    added Home component

---
+++ reactportfolio/src/components/Home/index.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import Loader from "react-loader";
+import {Link} from "react-router-dom";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import LogoTitle from "../../assets/images/logo-s.png";
+import Logo from './Logo';
