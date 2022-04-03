0f4c23b4dc621b7c6ab83842eaad9768f0686953
Muhammad Asif <asif88383@gmail.com>
2022-04-03 12:16:49 -0700

    some bug fixes

--- reactportfolio/src/components/Home/index.js
+++ reactportfolio/src/components/Home/index.js
@@ -1,63 +1,75 @@
-import { useEffect, useState } from "react";
-import Loader from "react-loaders";
-import {Link} from "react-router-dom";
-import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss";
-import LogoTitle from "../../assets/images/logo-s.png";
-import Logo from './Logo';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from './Logo'
+import './index.scss'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState("text-animate");
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    const nameArray = ['A', 'S','I','F'];
-    const jobArray = ['R','E','A','C', 'T',' ','P','R','O','G','R','A','M','M','E','R'];
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLetterClass("text-animate-hover");
-        }, 4000);
-    }, []);
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
-    return (
-        <>
-            <div className="home-page container">
-                <div className="text-zone">
-                    <h1>
-                        <span className={letterClass}>H</span>
-                        <span className={ `${letterClass} _12`}>i,</span>
-                        <br />
-                        <span className={ `${letterClass} _13`}>I</span>
-                        <span className={ `${letterClass} _14`}>'m</span>
-                        <img
-                            src={LogoTitle}
-                            alt="Mern Stack Developer Asif"
-                        />
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
 
-                        <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={nameArray}
-                            idx={15}
-                        />
-                        <br />
-
-                        <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={jobArray}
-                            idx={22}
-                        />
-                    </h1>
-
-                    <h2>MERN Stack Developer | React Developer | Python Programmer</h2>
-                    <Link to="/contact" className="flat-button">
-                        Contact Me
-                    </Link>
-                </div>
-                <Logo />
-            </div>
-
-            <Loader type="pacman" />
-        </>
-    );
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Home;
