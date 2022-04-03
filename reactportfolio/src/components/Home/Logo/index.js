12bce5d498d8e570a474ff7345b1840360d6c92a
Muhammad Asif <asif88383@gmail.com>
2022-04-03 03:46:36 -0700

    added Home/Logo component

---
+++ reactportfolio/src/components/Home/Logo/index.js
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+import gsap from "gsap-trial";
+import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
+import Logos from '../../assets/images/logo-s.png';
+import './index.scss';
+
